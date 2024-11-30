db18c03eb3eb713c45dd02018b6566e4a5e1657c
Narin251 <s244717@student.dtu.dk>
2024-11-30 10:15:52 +0100

    Update hardware-projekt.ino
    removed things related to touch screen + added timer interrupt for checking win condition

--- hardware-projekt/hardware-projekt.ino
+++ hardware-projekt/hardware-projekt.ino
@@ -1,40 +1,11 @@
+#include <TimerOne.h> //for the timer interrupt
 #include <Adafruit_GFX.h> //library used for drawing shapes 
 #include <MCUFRIEND_kbv.h> //library used for the screen 
-#include <TouchScreen.h> //library used for touchscreen
 #include <LiquidCrystal.h> //this is for the second display we used 
 MCUFRIEND_kbv tft;
 
 LiquidCrystal lcd(25, 27, 35, 33, 31, 29);
-
-int time = 0;//this is for testing the screen 
-
-
-
-// ALL Touch panels and wiring is DIFFERENT
-// copy-paste results from TouchScreen_Calibr_native.ino
-const int XP = 6, XM = A2, YP = A1, YM = 7; //ID=0x9341
-
-//touchscreen
-TouchScreen ts = TouchScreen(6, A1, A2, 7, 300);
-#define MINPRESSURE 200
-#define MAXPRESSURE 1000
-const int TS_LEFT = 907, TS_RT = 136, TS_TOP = 942, TS_BOT = 139;
-
-int pixel_x, pixel_y; //Touch_getXY() updates global vars
-bool Touch_getXY(void)
-{
-TSPoint p = ts.getPoint();
-pinMode(YP, OUTPUT); //restore shared pins
-pinMode(XM, OUTPUT);
-digitalWrite(YP, HIGH); //because TFT control pins
-digitalWrite(XM, HIGH);
-bool pressed = (p.z > MINPRESSURE && p.z < MAXPRESSURE);
-if (pressed) {
-pixel_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width()); //.kbv makes sense to me
-pixel_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
-}
-return pressed;
-}
+ 
 
 #define BLACK 0x0000
 #define BLUE 0x001F
@@ -46,7 +17,6 @@ return pressed;
 #define WHITE 0xFFFF
 
 
-
 //joystick
 int xPin = A8;
 int yPin= A9;
@@ -54,11 +24,11 @@ int buttonPin = 23;
 int xVal;  // how far is x axis
 int yVal; //how far is y axis
 int buttonState; //is the button pushed (is this for staring the game?)
-
 bool buttonPressed = false;  // Flag to track if button is pressed
 bool lastButtonState = HIGH;  // Last button state (using INPUT_PULLUP, HIGH means button not pressed)
 unsigned long lastDebounceTime = 0;  // Last time button state changed
 unsigned long debounceDelay = 50;  // debounce delay time (in milliseconds)
+
 //interrupt 
 volatile bool gameStarted = false;  // indicate if the game has started
 
@@ -69,15 +39,16 @@ int ghostX = CELL_SIZE * 6, ghostY = CELL_SIZE * 4; // Ghost's initial position
 int score = 0; // Track score
 
 //speaker
-const int SPEAKER = 53;
+const int SPEAKER= 29;
 
-#DEFINE NOTE_C 65
+
+
 
 void setup(void){
 
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
-  lcd.print("Time:");
+  lcd.print("your Score");
 
 //joystick 
 
@@ -90,10 +61,10 @@ pinMode(buttonPin, INPUT_PULLUP);
   tft.begin(ID);
   tft.setRotation(1); // LANDSCAPE
   tft.fillScreen(BLACK);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP); //joystick 
   pinMode(SPEAKER, OUTPUT);
 
-  // the button is connected to a pullup resistor, meaning that, when not pressed the value is high, this means, that when you press it the value will be low (hence FALLING)
+  // the jostick(button) is connected to a pullup resistor, meaning that, when not pressed the value is high, this means, that when you press it the value will be low (hence FALLING)
 
   attachInterrupt(digitalPinToInterrupt(buttonPin), startGame, FALLING);
   tft.fillScreen(BLACK);
@@ -106,7 +77,13 @@ pinMode(buttonPin, INPUT_PULLUP);
   tft.print("to start the game.");
 
 
+  Timer1.initialize(100); //this is our timer interurpt for checking the score
+  Timer1.attachInterrupt(wonGame); //when 100ms passed call check win condition so that the system doesnt get overloaded
+
+
 }
+
+
 
 void loop() {
 
@@ -134,19 +111,13 @@ void loop() {
 
   }
 
-lastButtonState = currentButtonState;
-
-
-  
-
+ lastButtonState = currentButtonState;
 
   if(gameStarted){
-
   //joystick
   xVal= analogRead(xPin);
   yVal = analogRead(yPin);
   
-
   //testing joystick 
   Serial.print("X: ");
   Serial.print(xVal);
@@ -156,16 +127,26 @@ lastButtonState = currentButtonState;
   Serial.print(buttonState);
   delay(100);
 
-
   movePacWJoy();
   trackingScores(); 
+
+  //external LCD
+  lcd.setCursor(0, 1);      
+  lcd.print("Score: ");     
+  lcd.print(score);         
+
   moveGhost();
   checkGhostCollision();
 
-  lcd.setCursor(0,1);
-  lcd.print(time);
-  
-  time++;
+  if (wonGame()) {
+      tone(SPEAKER, 700, 1000); // Play a winning sound
+      tft.setCursor(50, 150);
+      tft.setTextColor(GREEN);
+      tft.setTextSize(2);
+      tft.print("You Win!");
+      gameStarted = false; // Stop the game
+    }
+
   
 
   } else {
@@ -179,15 +160,6 @@ lastButtonState = currentButtonState;
     tft.setCursor(50, 180);
     tft.print("to start/restart the game.");
   }
-
-  //this is for the second screen 
-  lcd.setCursor(0,1);
-  lcd.print(time);
-  //delay (1000);
-  time++;
-
- 
-
 
 }
 
@@ -298,30 +270,48 @@ void moveGhost(){
   
   clearGhost(); //clearing the maze so we dont end up having two ghosts
 
+  int newGhostX = ghostX;
+  int newGhostY = ghostY;
+
   int direction = random(4); //0=up 1= down 2= right 3=left /(-1) is there because we count from 0 / it should not be 1, 1 is wall in the maze!
 
    int ghostCol = ghostX / CELL_SIZE; //ghosts col 
    int ghostRow = ghostY / CELL_SIZE; //ghosts row
 
-  if((direction==0) && (ghostRow > 0) && (maze[ghostX / CELL_SIZE][(ghostY / CELL_SIZE) - 1] != 1)){
-    ghostY -= CELL_SIZE;
-  }
-  if((direction==1) && (ghostRow < ROWS - 1) && (maze[ghostX / CELL_SIZE][(ghostY / CELL_SIZE) + 1] != 1)){
-    ghostY+= CELL_SIZE;
-  }
-  if((direction==2) && (ghostCol < COLS - 1) && (maze[ghostX / CELL_SIZE +1][(ghostY / CELL_SIZE) ] != 1)){
-    ghostX+= CELL_SIZE;
-  }
-    if((direction==3) && (ghostCol > 0) && (maze[ghostX / CELL_SIZE -1][(ghostY / CELL_SIZE) ] != 1)){
-    ghostX-= CELL_SIZE;
-  }
+   if(direction==0){
 
+    newGhostY-= CELL_SIZE;
+
+   } else if(direction==1){
+
+     newGhostY+= CELL_SIZE;
+
+   } else if(direction==2){
+
+     newGhostX+= CELL_SIZE;
+
+   } else if(direction==3){
+
+     newGhostX-= CELL_SIZE;
+   }
+
+   int newRow = newGhostY / CELL_SIZE;
+   int newCol = newGhostX / CELL_SIZE;
+
+
+   if (newRow >= 0 && newRow < ROWS && newCol >= 0 && newCol < COLS) {
+            if (maze[newRow][newCol] != 1) {  // 1 is the wall
+                // Update ghost position if not a wall
+                ghostX = newGhostX;
+                ghostY = newGhostY;
+            }
+    }
   drawGhost();
   lastMoveTimeGhost = currentTimeGhost;
-
-
   }
 }
+
+ 
 
 //deteccting collision
 
@@ -412,6 +402,20 @@ void movePacWJoy() {
 }
 
 
+//checking if you won the game: 
+
+bool wonGame() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (maze[i][j] == 2) {
+                return false; 
+            }
+        }
+    }
+    return true; 
+}
+
+
 
 
 
@@ -421,6 +425,7 @@ void movePacWJoy() {
  
 
 
+ 
 
 
 
